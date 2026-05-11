8f86da22818ecf703297dbb2509e8d83e36439e7
ablueone <vls_89@hotmail.com>
2026-05-11 11:42:28 +0200

    Fix CORS for Vercel

--- bokdags/backend/config/middlewares.js
+++ bokdags/backend/config/middlewares.js
@@ -2,7 +2,19 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'https://bok-dags.vercel.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
