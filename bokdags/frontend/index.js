68a33e356ac8ad6dbc071443cb819c5585fd18ca
ablueone <vls_89@hotmail.com>
2026-05-11 16:38:55 +0200

    Fix Strapi Cloud API URL

--- bokdags/frontend/index.js
+++ bokdags/frontend/index.js
@@ -1,4 +1,4 @@
-const API_URL = "https://artistic-hero-019f59ea0e.strapiapp.com/admin";
+const API_URL = "https://artistic-hero-019f59ea0e.strapiapp.com";
 const API = `${API_URL}/api`;
 
 let books = [];
